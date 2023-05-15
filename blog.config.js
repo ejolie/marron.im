4764a4f4723f53a8452d2f3688828176e17d9d78
Eunjeong Park <ejolie.dev@gmail.com>
2023-05-16 03:12:40 +0900

    Remove description

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: '마롱',
   email: 'ejolie.dev@gmail.com',
   link: 'https://marron.im',
-  description: 'Faites des bêtises, mais faites-les avec enthousiasme.',
+  description: '',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Seoul', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
